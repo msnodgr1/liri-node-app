50c6b73d2b380972aea1659a18c38e4ae848c720
Mark Snodgrass <msnodgr1@gmail.com>
2017-09-24 21:37:11 -0400

    Initial commit

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'izXjLvO4Tgte3WBIPjeVKCEPg',
+  consumer_secret: 'DjzWLZXzTAuIsp8TJoGbcPrSnYx4VHUXqoPru8CTuG6xnlhnkN',
+  access_token_key: '912120169614528513-pARMQMFtIJKzjnQdBRtUUgkVzW9hIqP',
+  access_token_secret: 'NvbGUTXlrE0bAR9nTq20kR1iZ9gFtDZVyOxhkpE8VC69P',
+}
+
+module.exports = twitterKeys;
